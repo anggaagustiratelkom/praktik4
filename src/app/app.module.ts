af0ef44d7caad4514bbc5ca8369d7427b6d644ba
anggaagustiratelkom <anggaagustira@gmail.com>
2023-12-19 15:01:39 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailComponent } from './detail/detail.component';
+import { DashboarComponent } from './dashboar/dashboar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DetailComponent, DashboarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
