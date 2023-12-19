81b1de75ca0dbaa3aa669c19cbeeb5d2a6b5da30
anggaagustiratelkom <anggaagustira@gmail.com>
2023-12-19 16:09:42 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboarComponent } from './dashboar/dashboar.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'dashboar',
+  },
+  {
+    path: 'dashboar',
+    component: DashboarComponent,
+  },
+  {
+    path: 'detail',
+    component: DetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/dashboar/dashboar.component.ts
+++ src/app/dashboar/dashboar.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboar',
   templateUrl: './dashboar.component.html',
-  styleUrls: ['./dashboar.component.scss']
+  styleUrls: ['./dashboar.component.scss'],
 })
 export class DashboarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
